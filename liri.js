aa3b9316539aafd1c6c9911f50266d71bcd77f09
Prudence Codjia <pcodjia@ironguard.net>
2019-01-08 23:16:07 -0500

    adding more lines

--- liri.js
+++ liri.js
@@ -6,3 +6,37 @@ var axios = require("axios");
 var input = process.argv[2];
 var mySearch = process.argv[3];
 
+startLiri(mySearch, input);
+
+
+function startLiri(searchValue, command) {
+    for (var i = 4; i < process.argv.length; i++) {
+        if (i < process.argv.length) {
+            mySearch = mySearch + " ";
+        }
+       mySearch = mySearch + process.argv[i];
+    }
+    doSearch(mySearch, input);
+}
+
+function doSearch(mySearch, input) {
+    switch (input) {
+        case 
+        "concert-this":
+            bandsInTown(mySearch);
+            break;
+        case "spotify-this-song":
+            spotifyThisSong(mySearch);
+           
+            break;
+        case "movie-this":
+            movieThis(mySearch);
+            
+            break;
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+        default:
+            console.log(input + ": Don't recongnize this input");
+    }
+}
