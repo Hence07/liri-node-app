7c999d38c73991b2e4fc011131452bee226e8e52
Prudence Codjia <pcodjia@ironguard.net>
2019-01-08 23:50:32 -0500

    testfile.js

---
+++ testfile.js
@@ -0,0 +1,17 @@
+var fs = require("fs");
+
+fs.readFile("random.txt", "utf8", function (error, data) {
+
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+        return console.log(error);
+    }
+
+    // console.log(data);
+
+    var dataArr = data.split(",");
+
+    console.log(dataArr[0]);
+    console.log(dataArr[1]);
+
+});
